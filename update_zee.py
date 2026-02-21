54f45f87cbe6f52248317056dc1aa6f0546a39f8
Ashish <138308243+rathee-ashish@users.noreply.github.com>
2026-02-21 20:59:46 +0530

    Update update_zee.py

--- update_zee.py
+++ update_zee.py
@@ -15,7 +15,7 @@ import requests
 MY_PLAYLIST = "my_playlist.m3u"
 ZEE_CHANNELS_FILE = "zeechannels.txt"
 # ⚠️ Use RAW link, not the GitHub page link:
-ZEE_SOURCE_URL = "https://raw.githubusercontent.com/alex8875/m3u/refs/heads/main/z5.m3u"
+ZEE_SOURCE_URL = "https://join-vaathala1-for-more.vodep39240327.workers.dev/zee5.m3u"
 
 
 def parse_channels_file(path):
