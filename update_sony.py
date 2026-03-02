a3ee06712ecd9b97bf70bd55436e9fc02c5cecde
Ashish <138308243+rathee-ashish@users.noreply.github.com>
2026-03-02 21:31:33 +0530

    Update update_sony.py

--- update_sony.py
+++ update_sony.py
@@ -13,7 +13,7 @@ import requests
 
 MY_PLAYLIST = "my_playlist.m3u"
 SONY_CHANNELS_FILE = "sonychannels.txt"
-SONY_SOURCE_URL = "https://solii.saqlainhaider8198.workers.dev/"
+SONY_SOURCE_URL = "https://raw.githubusercontent.com/Sflex0719/SonGharLive/refs/heads/main/SL.m3u"
 
 
 def parse_channels_file(path):
